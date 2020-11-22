e4e288cdc4635047b275d6a5e501dd6d36ef76a0
Miguel <miguel.fiandor.gutierrez@gmail.com>
2020-11-22 22:40:59 +0100

    ign data folder

---
+++ descarga_tcu_informes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p data/tcu
+
+for i in {1000..1005}; do
+    filenpath="data/tcu/I$i.pdf"
+    if [ ! -f "$filenpath" ]; then
+        curl "https://www.tcu.es/repositorio/b4a6827c-5afd-476c-b3b0-7e61f6773ea9/I$i.pdf" -o "$filenpath"
+        sleep 0.2
+    fi
+done
